1d47be7559a9a4d5dc58bdd24fe5949eeeeabf7c
chrbour <108238838+chrbour@users.noreply.github.com>
2023-06-25 09:53:27 +0200

    Rectification images Slideshow et banner avec Object--fit:cover, width:100% et height:100%

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -7,22 +7,20 @@ function Slideshow ({images}){
         sens===-1? (photo>0? updatePhoto(photo-1) : updatePhoto(nbrePhotos-1)) : (photo<nbrePhotos-1? updatePhoto(photo+1) : updatePhoto(0));
     }    
     return (
-        <Fragment>
-            <div className='containerPhotos'>
-                {nbrePhotos>1? 
-                <Fragment>
-                        <p className="paginationPhotos">{(photo+1)+'/'+nbrePhotos}</p> 
+        <div className='containerPhotos'>
+            {nbrePhotos>1? 
+            <Fragment>
+                <p className="paginationPhotos">{(photo+1)+'/'+nbrePhotos}</p> 
                     <div className="containerChevrons">
                         <p className="chevronGauche" onClick={() => changePhoto(-1)}><i class="fa-solid fa-chevron-left"></i></p>
                         <p className="chevronDroit" onClick={() => changePhoto(1)}><i class="fa-solid fa-chevron-right"></i></p>
                     </div>
-                </Fragment>
-                : null }
-                <div>
-                 <img src={images[photo]} alt="Photo de la location"></img>   
-                </div>  
-             </div>          
-        </Fragment>  
+            </Fragment>
+            : null }
+            
+            <img className="photosSlideshow" src={images[photo]} alt="Photo de la location"></img>   
+            
+        </div>          
     )
 }
 
